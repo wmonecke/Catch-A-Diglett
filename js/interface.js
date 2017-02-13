cd74d671db9f79406370c188788525f0511955fb
Walter Monecke <wmonecke@gmail.com>
2017-02-13 18:45:05 -0500

    Added Game Logic and Interface

--- js/interface.js
+++ js/interface.js
@@ -1,1 +1,78 @@
-console.log('interface');
+var myGame;
+
+$(document).ready(function(){
+
+    //Update Score
+  var score = 0;
+
+  $('#container').on('click', '.mole1', function(){
+    score++;
+    $('#score').html(score);
+    $(this).removeClass('mole1');
+    myGame.removeMole($(this).data('row'), $(this).data('col'));
+
+  });
+
+  $('#container').on('click', '.mole2', function(){
+    score = score - 3;
+    $('#score').html(score);
+    $(this).removeClass('mole2');
+    myGame.removeMole($(this).data('row'), $(this).data('col'));
+
+  });
+
+  function startGame(){
+    setInterval(function(){
+      myGame.wildDiglettAppears();
+      renderBoard();
+    }, 400);
+  }
+
+  myGame = new Game('Walter');
+
+  //Render Board
+  function renderBoard () {
+    $('#container').empty();
+    myGame.board.forEach(function (row, rowIndex) {
+      row.forEach(function (col, colIndex) {
+        var pokemon;
+
+        if (col === 1) {
+          pokemon = 'mole1';        //Diglett
+        } else if (col === 2) {
+          pokemon = 'mole2';        // Dugtrio
+        } else {
+          pokemon = '';             //Grass
+        }
+
+        var slotHtml = '<div class="hole '+pokemon+'" data-row="'+rowIndex+'" data-col="'+colIndex+'"></div>';
+        $('#container').append(slotHtml);
+      });
+    });
+  }
+
+
+  //Start Game on Start press.
+  $('#start').on('click', function(){
+    startGame();
+  });
+
+  //Instructions-Button Pop Up
+  $('#instructions').on('click', function(e){
+    e.stopPropagation();
+    $('#popup').fadeIn(300, function(){
+
+      $('body').on('click', function(){
+        e.stopPropagation();
+        $('#popup').fadeOut(300);
+      });
+    });
+  });
+
+  //Start-Button Pop Up
+
+});
+
+
+
+//Render Board
--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,54 @@
-console.log('logic');
+//Constructor for every Game
+function Game (player) {
+  this.player = player;
+  this.board = [
+    [null, null, null, null, null, null, null, null, null, null], //0
+    [null, null, null, null, null, null, null, null, null, null], //1
+    [null, null, null, null, null, null, null, null, null, null], //2
+    [null, null, null, null, null, null, null, null, null, null], //3
+    [null, null, null, null, null, null, null, null, null, null], //4
+    [null, null, null, null, null, null, null, null, null, null], //5
+    [null, null, null, null, null, null, null, null, null, null], //6
+    [null, null, null, null, null, null, null, null, null, null], //7
+    [null, null, null, null, null, null, null, null, null, null], //8
+  ];
+  this.diglett = 1;
+  this.dugtrio = 2;
+  this.score = 0;
+}
+
+//Choose one random Index in a random Column - Function
+Game.prototype.wildDiglettAppears = function() {
+
+  //Picking a random Bush for a DIGLETT, by picking a random Row and then a random Column inside:
+  var randomRow = Math.floor(Math.random() * this.board.length);
+  var randomCol = Math.floor(Math.random() * this.board[randomRow].length);
+  var randomDiglett = this.board[randomRow][randomCol];
+  // console.log('My random Bush for Diglett is located at Row: ' + randomRow + ' and Column: ' + randomCol);
+
+  //Picking a random Bush for a DUGTRIO, by picking a random Row and then a random Column:
+  var randomRow2 = Math.floor(Math.random() * this.board.length);
+  var randomCol2 = Math.floor(Math.random() * this.board[randomRow].length);
+  var randomDugtrio = this.board[randomRow2][randomCol2];
+  // console.log('My random Bush for Dugtrio is located at Row: ' + randomRow2 + ' and Column: ' + randomCol2);
+
+  //Inserting one random (Diglett = 1) and one random (Dugtrio = 2) into the array
+  this.board[randomRow][randomCol] = 1;
+  this.board[randomRow2][randomCol2] = 2;
+
+  // console.log('My first board: ' + this.board);
+
+  var that = this;
+  setTimeout(function(){
+    if (that.board[randomRow][randomCol] === 1 || that.board[randomRow2][randomCol2] === 2) {
+      that.board[randomRow][randomCol] = null;
+      that.board[randomRow2][randomCol2] = null;
+      // console.log('My second board: ' + that.board);
+    }
+  }, 1000);
+
+};
+
+Game.prototype.removeMole = function (row, column) {
+    this.board[row][column] = null;
+};
